18d96787abb2a2aef49d8312a61e5a8b4da04e19
Ashlee Barksdale <ashleelennab@gmail.com>
2020-06-15 22:32:41 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,12 +1,11 @@
 # don't forget to add: require 'pry'
 require 'pry'
 
-def start_game(player1 = Rocky, player2 = Bullwinkle)
-  "Hello #{player1} & #{playar2}"
+def start_game(player1, player2)
+  "Hello #{player1} & #{player2}"
   
 end
 
 def play_game(player1, player2)
   "#{player1} is better than #{player2}"
-  player1 Rocky player2 Bullwinkle
 end
